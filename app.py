d1de7b553ea44b0a00a42c5a77f00b227c7269c5
Harshit Shukla <hrshtshkl5@gmail.com>
2026-03-01 15:21:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import requests, io, json, base64, pandas as pd
 from google import genai
@@ -10,20 +11,27 @@ from streamlit_gsheets import GSheetsConnection
 # --- SETUP ---
 st.set_page_config(page_title="Sirosri AI", page_icon="🤖")
 
+
+client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 # Securely load API Key from Secrets
 try:
-    genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-    model = genai.GenerativeModel('gemini-1.5-flash')
     conn = st.connection("gsheets", type=GSheetsConnection)
 except Exception as e:
-    st.error("Setup Error: Check your secrets.toml file!")
-    st.stop()
+    st.error("Setup Error: Check your secrets.toml gsheet file!")
 
-def load_lottie(url):
-    return requests.get(url).json()
+
+def load_lottie(filename):
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+    except json.JSONDecodeError:
+        print("Error: Failed to decode JSON from the file (malformed JSON).")
 
 # Assets
-ANIM_URL = "https://lottie.host/866d9203-39d2-4309-8088-3e477e64177d/EunE5nIqj8.json"
+ANIM_URL = "animebot.json"
 
 SURVEY_CONFIG = [
     {"id": "name", "q": "I'm Sirosri! What's your name?", "type": "personalized"},
@@ -34,7 +42,7 @@ SURVEY_CONFIG = [
 # --- SESSION LOGIC ---
 if "step" not in st.session_state:
     st.session_state.step, st.session_state.answers, st.session_state.history = 0, {}, []
-    st.session_state.chat = model.start_chat(history=[])
+    st.session_state.chat = client.chats.create(model='gemini-2.0-flash')
 
 # --- UI ---
 with st.sidebar:
@@ -68,7 +76,14 @@ if st.session_state.step < len(SURVEY_CONFIG):
     user_val = None
     if u_audio:
         with st.spinner("Sirosri is listening..."):
-            res = model.generate_content(["Transcribe:", {"mime_type": "audio/wav", "data": u_audio.getvalue()}])
+            res = client.models.generate_content(
+                model='gemini-2.0-flash',
+                contents=[
+                    u_audio.getvalue(),
+                    "Transcribe this audio exactly, including speaker labels and timestamps."
+                ]
+            )
+            # res = model.generate_content(["Transcribe:", {"mime_type": "audio/wav", "data": u_audio.getvalue()}])
             user_val = res.text
     elif u_text:
         user_val = u_text
